6e9ba9402becff23e52699dd17de23567914663e
SteveOC <steven.oconnor@neds.com>
2019-05-15 17:00:40 +1000

    oob compiler prep

--- cmd/republique/main.go
+++ cmd/republique/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/steveoc64/republique5/republique"
+	"os"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,6 +11,17 @@ import (
 const version = "0.1"
 
 func main() {
+	// get the command verb and process
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "compile":
+		compile(os.Args[2:])
+	case "serve":
+		serve(os.Args[2:])
+	}
 	port := flag.Int("port", 1815, "port number to run on")
 	webport := flag.Int("webport", 8080, "port number to run on")
 	flag.Parse()
