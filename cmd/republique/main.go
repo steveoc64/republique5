b9862dd2b505c2d3b5895202199128886b20bee1
steveoc <steveoc64@gmail.com>
2019-05-16 21:51:40 +1000

    thats better

--- cmd/republique/main.go
+++ cmd/republique/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
@@ -31,24 +30,12 @@ func main() {
 	case "compile":
 		compile(log, os.Args[2:])
 	case "serve":
-		f := flag.NewFlagSet("serve args", flag.ContinueOnError)
-		gamename := ""
-		port := 1815
-		web := 8080
-		f.IntVar(&port, "port", 1815, "port number to run RPC server on")
-		f.IntVar(&web, "web", 8080, "port number to run web app on")
-		f.StringVar(&gamename, "game", "game.game", "filename of the game to run")
-		err := f.Parse(os.Args[2:])
+		err := serve(log, os.Args[2:])
 		if err != nil {
-			println(err.Error())
+			log.WithError(err).Println("Incorrect Server Args", os.Args[2:])
 			usage()
 			os.Exit(1)
 		}
-
-		println("set gamename to", gamename)
-		println("set port to", port)
-		println("set web to", web)
-		serve(log, gamename, port, web)
 	default:
 		usage()
 	}
--- cmd/republique/serve.go
+++ cmd/republique/serve.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
 	"github.com/sirupsen/logrus"
 	"github.com/steveoc64/republique5/republique"
+	"os"
 )
 
-func serve(log *logrus.Logger, gamename string, port int, web int) {
+func serve(log *logrus.Logger, args []string) error {
+	f := flag.NewFlagSet("serve args", flag.ContinueOnError)
+	gamename := ""
+	port := 1815
+	web := 8080
+	f.IntVar(&port, "port", 1815, "port number to run RPC server on")
+	f.IntVar(&web, "web", 8080, "port number to run web app on")
+	f.StringVar(&gamename, "game", "game.game", "filename of the game to run")
+	err := f.Parse(os.Args[2:])
+	if err != nil {
+		return err
+	}
 	s := republique.NewRServer(log, version, gamename, port, web)
 	s.Serve()
+	return nil
 }
