cd34038f89eefeea768bc0939c5542f193b7b18e
steveoc <steveoc64@gmail.com>
2019-05-27 00:18:31 +1000

    rm debug

--- gui/appwindow/phaser.go
+++ gui/appwindow/phaser.go
@@ -12,8 +12,7 @@ func (a *App) Phaser() {
 		for {
 			time.Sleep(time.Second * 3)
 
-			t1 := time.Now()
-			gameTime, err := a.client.GameTime(context.Background(), &rp.StringMessage{Value: a.Token})
+			_, err := a.client.GameTime(context.Background(), &rp.StringMessage{Value: a.Token})
 			if err != nil {
 				println("got an error", err.Error())
 			}
